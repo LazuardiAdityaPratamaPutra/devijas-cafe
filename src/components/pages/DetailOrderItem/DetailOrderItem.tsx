13e457a4e07a1e34bde36f23e4ddf52f5d8dd466
LazuardiAdityaPratamaPutra <lazuardiaditya17@gmail.com>
2026-09-12 06:09:45 +0700

    nambah card time dan queue di pages detail order item

--- src/components/pages/DetailOrderItem/DetailOrderItem.tsx
+++ src/components/pages/DetailOrderItem/DetailOrderItem.tsx
@@ -8,6 +8,7 @@ import NavbarAdmin from "../../ui/NavbarAdmin";
 import { FaArrowLeft } from "react-icons/fa6";
 import { FiPrinter } from "react-icons/fi";
 import profilCowok from "../../../assets/profil-customer-cowok.jpg";
+import { CiClock2 } from "react-icons/ci";
 
 const DetailOrderItem = () => {
   const { id } = useParams();
@@ -62,6 +63,48 @@ const DetailOrderItem = () => {
               <h4>2 Guests</h4>
             </div>
           </div>
+
+          <div className={styles.line}></div>
+
+          <section className={styles.identCustomer}>
+            <div className={styles.identLeft}>
+              <img
+                src={profilCowok}
+                alt="Profil Customer Cowok"
+                className={styles.avatar}
+              />
+
+              <div className={styles.customerDetails}>
+                <div className={styles.nameAndBadge}>
+                  <h6>{orders?.customer_name}</h6>
+                  <span className={styles.badgeMember}>SILVER</span>
+                </div>
+                <p className={styles.phoneNumber}>+62 812-4910-9921</p>
+              </div>
+            </div>
+
+            <div className={styles.rewardRight}>
+              <span className={styles.rewardLabel}>REWARDS</span>
+              <p className={styles.rewardPts}>1,450 pts</p>
+            </div>
+          </section>
+
+          <section className={styles.cardTimeAndBarQueue}>
+            <div className={styles.cardTime}>
+              <h6>Preparation Time</h6>
+              <div className={styles.minute}>
+                <CiClock2 className={styles.iconClock}/>
+                <p>~8 ~10 mins</p>
+              </div>
+            </div>
+            <div className={styles.cardQueue}>
+              <h6>Bar Queue Pos</h6>
+              <div className={styles.queue}>
+                <span className={styles.amberDot}></span>
+                <p>#2 in Queue</p>
+              </div>
+            </div>
+          </section>
         </section>
 
         <section className={styles.rightCard}></section>
