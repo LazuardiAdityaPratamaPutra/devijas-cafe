9a0bdf1c04e9eb7eca3aee0e3d68a479e815b05f
LazuardiAdityaPratamaPutra <lazuardiaditya17@gmail.com>
2026-09-12 10:49:59 +0700

    nambah card modifiers di pages detail order item

--- src/components/pages/DetailOrderItem/DetailOrderItem.tsx
+++ src/components/pages/DetailOrderItem/DetailOrderItem.tsx
@@ -50,61 +50,100 @@ const DetailOrderItem = () => {
         </section>
 
         <section className={styles.leftCard}>
-          <div className={styles.containerHeaderLeftCard}>
-            <div className={styles.serviceType}>
-              <h6>SERVICE TYPE</h6>
-              <div className={styles.titleOrderTable}>
-                <h4>Dine-in Order</h4>
-                <span>Table {orders?.table_number}</span>
+          <div className={styles.leftCardIdent}>
+            <div className={styles.containerHeaderLeftCard}>
+              <div className={styles.serviceType}>
+                <h6>SERVICE TYPE</h6>
+                <div className={styles.titleOrderTable}>
+                  <h4>Dine-in Order</h4>
+                  <span>Table {orders?.table_number}</span>
+                </div>
+              </div>
+              <div className={styles.customer}>
+                <h6>CUSTOMER</h6>
+                <h4>2 Guests</h4>
               </div>
             </div>
-            <div className={styles.customer}>
-              <h6>CUSTOMER</h6>
-              <h4>2 Guests</h4>
+
+            <div className={styles.line}></div>
+
+            <div className={styles.identCustomer}>
+              <div className={styles.identLeft}>
+                <img
+                  src={profilCowok}
+                  alt="Profil Customer Cowok"
+                  className={styles.avatar}
+                />
+
+                <div className={styles.customerDetails}>
+                  <div className={styles.nameAndBadge}>
+                    <h6>{orders?.customer_name}</h6>
+                    <span className={styles.badgeMember}>GOLD</span>
+                  </div>
+                  <p className={styles.phoneNumber}>+62 812-4910-9921</p>
+                </div>
+              </div>
+
+              <div className={styles.rewardRight}>
+                <span className={styles.rewardLabel}>REWARDS</span>
+                <p className={styles.rewardPts}>1,450 pts</p>
+              </div>
+            </div>
+
+            <div className={styles.cardTimeAndBarQueue}>
+              <div className={styles.cardTime}>
+                <h6>Preparation Time</h6>
+                <div className={styles.minute}>
+                  <CiClock2 className={styles.iconClock} />
+                  <p>~8 ~10 mins</p>
+                </div>
+              </div>
+              <div className={styles.cardQueue}>
+                <h6>Bar Queue Pos</h6>
+                <div className={styles.queue}>
+                  <span className={styles.amberDot}></span>
+                  <p>#2 in Queue</p>
+                </div>
+              </div>
             </div>
           </div>
 
-          <div className={styles.line}></div>
+          <div className={styles.cardQuickModifiers}>
+            <div className={styles.headerModifiers}>
+              <h3>QUICK MODIFIERS & ADD-ONS</h3>
+              <span>FAST TOUCH</span>
+            </div>
 
-          <section className={styles.identCustomer}>
-            <div className={styles.identLeft}>
-              <img
-                src={profilCowok}
-                alt="Profil Customer Cowok"
-                className={styles.avatar}
-              />
+            <div className={styles.line}></div>
 
-              <div className={styles.customerDetails}>
-                <div className={styles.nameAndBadge}>
-                  <h6>{orders?.customer_name}</h6>
-                  <span className={styles.badgeMember}>SILVER</span>
-                </div>
-                <p className={styles.phoneNumber}>+62 812-4910-9921</p>
+            <div className={styles.containerCardChoice}>
+              <div>
+                <h6>Extra Shot</h6>
+                <p>+ Rp 5.000</p>
               </div>
-            </div>
-
-            <div className={styles.rewardRight}>
-              <span className={styles.rewardLabel}>REWARDS</span>
-              <p className={styles.rewardPts}>1,450 pts</p>
-            </div>
-          </section>
-
-          <section className={styles.cardTimeAndBarQueue}>
-            <div className={styles.cardTime}>
-              <h6>Preparation Time</h6>
-              <div className={styles.minute}>
-                <CiClock2 className={styles.iconClock}/>
-                <p>~8 ~10 mins</p>
+              <div>
+                <h6>Oat Milk Swap</h6>
+                <p>+ Rp 6.000</p>
               </div>
-            </div>
-            <div className={styles.cardQueue}>
-              <h6>Bar Queue Pos</h6>
-              <div className={styles.queue}>
-                <span className={styles.amberDot}></span>
-                <p>#2 in Queue</p>
+              <div>
+                <h6>Less Ice 50%</h6>
+                <p>Standar Ice</p>
               </div>
+              <div>
+                <h6>Extra Hot</h6>
+                <p>+ Rp 0</p>
+              </div>
+              <div>
+                <h6>Vanilla Bean Syrup</h6>
+                <p>+ Rp 4.000</p>
+              </div>
+              <div>
+                <h6>Takeaway Packaging</h6>
+                <p>+ Rp 2.000</p>
+              </div>
+              
             </div>
-          </section>
+          </div>
         </section>
 
         <section className={styles.rightCard}></section>
